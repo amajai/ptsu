8e044ac02af3f0c3378badf84e48c350678fccbd
tannerkrewson <tannerkrewson@gmail.com>
2018-05-09 22:50:17 -0500

    Corrected some grammars

--- src/app/intro/en-intro-dialogue.ts
+++ src/app/intro/en-intro-dialogue.ts
@@ -2,7 +2,7 @@ export const EN_INTRO_DIALOGUE: string[] = [
 	'Everyone knows that companies like Google, Apple, Facebook, Microsoft, and Amazon violate privacy and sell your data to advertisers without your permission.',
 	'But, you may not have known that Apple and Google have been installing a secret microchip in all Android and iPhones since 2012.',
 	'It is called the PTSU, which stands for Psychoelectric Telepathic Suggestion Chip.',
-	'The PTSU sends psychoelectric waves into your brain that change the things the brands that you prefer to buy without you ever knowing.',
+	'The PTSU sends psychoelectric waves into your brain that change the brands that you prefer to buy without you ever knowing.',
 	'The ability for advertisers to not just influence, but DECIDE which brand you buy has ushered in a new era of unethical advertising, unknown to anyone...',
 	'...until now.',
 	'My name is Tanner Krewson, and I have spent the last four years developing this website to uncover the biggest conspiracy of the last decade.',
